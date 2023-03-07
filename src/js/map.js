9bbcb5d1a67d986373739dbfc24536095bdaecac
Konstantin <kosvadmail@atomlc.com.ua>
2023-03-07 20:10:49 +0100

    Меняет размеры карт

--- src/js/map.js
+++ src/js/map.js
@@ -2,6 +2,12 @@ const mapIcons = document.querySelectorAll('.do-more-bg__img');
 const mediaQuery1024 = window.matchMedia('(min-width: 1024px)');
 
 const onMapIcon = (mapIcon) => {
+  const activeIcon = document.querySelector('.do-more-bg__img--active');
+  if (activeIcon !== null) {
+    activeIcon.classList.remove('do-more-bg__img--active');
+  }
+  mapIcon.classList.add('do-more-bg__img--active');
+
   const block = document.querySelector('.do-more__item');
   const blockImg = block.querySelector('img');
   const blockTitle = block.querySelector('.do-more__item-title');
@@ -21,6 +27,9 @@ const onMapIconUnHover = () => {
 };
 
 export const startMap = () => {
+  if (mediaQuery1024.matches) {
+    onMapIcon(mapIcons[0]);
+  }
   mapIcons.forEach((mapIcon) => {
     if (mediaQuery1024.matches) {
       mapIcon.addEventListener('mouseover', () => onMapIcon(mapIcon));
