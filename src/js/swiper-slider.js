affa42eeda6c0c66cf80b29e0762f823f24a6151
Konstantin <kosvadmail@atomlc.com.ua>
2023-05-09 15:47:29 +0200

    Добавляет пагинацию для слайдера и комментирует стрелки

--- src/js/swiper-slider.js
+++ src/js/swiper-slider.js
@@ -1,17 +1,23 @@
 // import Swiper JS
-import Swiper from 'swiper';
+import Swiper, { Pagination } from 'swiper';
 // import Swiper styles
-import 'swiper/css';
+import '../../node_modules/swiper/swiper-bundle.min.css';
 
 const container = document.querySelector('.swiper');
 // eslint-disable-next-line no-unused-vars
 const swiper = new Swiper(container, {
+  modules: [Pagination],
   loop: true,
   slidesPerView: 3,
   spaceBetween: 30,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
   },
   breakpoints: {
     400: {
@@ -22,21 +28,21 @@ const swiper = new Swiper(container, {
     },
 
   },
-  on: {
-    init: function () {
-      const prevEl = document.querySelector('.swiper-button-prev');
-      const nextEl = document.querySelector('.swiper-button-next');
+  //TODO on: {
+  //   init: function () {
+  //     const prevEl = document.querySelector('.swiper-button-prev');
+  //     const nextEl = document.querySelector('.swiper-button-next');
 
-      prevEl.addEventListener('click', (e) => {
-        e.preventDefault();
-        swiper.slidePrev();
-      });
+  //     prevEl.addEventListener('click', (e) => {
+  //       e.preventDefault();
+  //       swiper.slidePrev();
+  //     });
 
-      nextEl.addEventListener('click', (e) => {
-        e.preventDefault();
-        swiper.slideNext();
-      });
-    }
-  }
+  //     nextEl.addEventListener('click', (e) => {
+  //       e.preventDefault();
+  //       swiper.slideNext();
+  //     });
+  //   }
+  // }
 });
 
